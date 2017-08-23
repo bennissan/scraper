c75ef9715c6d4a5c4dc0d25b89c5700bfafeb2d0
bennissan <ben.nissan@tufts.edu>
2017-08-23 03:29:22 -0400

    Changed from searching in article html to searching in article title and text using newspaper.  Changed from setting explicit limit of 50 articles per news source to searching all articles from current day.  Split up behavior into functions.

--- scraper.py
+++ scraper.py
@@ -1,7 +1,10 @@
 import requests
-from bs4 import BeautifulSoup
+import datetime
 import newspaper
 import re
+
+code_words = ["Jew", "Zionist", "Globalist", "Elite", "Rothschild", "\(\(\(.\)\)\)", "Reptilian",
+             "New World Order", "Kabbal"]
 
 sources = [{"name": "Breitbart",         "url": "http://breitbart.com/"},
            {"name": "The Daily Caller",  "url": "http://dailycaller.com/"},
@@ -9,27 +12,43 @@ sources = [{"name": "Breitbart",         "url": "http://breitbart.com/"},
            {"name": "The Daily Wire",    "url": "http://www.dailywire.com/"},
            {"name": "National Review",   "url": "http://www.nationalreview.com/"}]
 
-codeWords = ["Jew", "Zionist", "Globalist", "Elite", "Rothschild", "\(\(\(.\)\)\)", "Reptilian",
-             "New World Order", "Kabbal"]
+papers = [newspaper.build(source["url"], memoize_articles=False, fetch_images=False) for source in sources]
+newspaper.news_pool.set(papers, threads_per_source=10)
+newspaper.news_pool.join()
+
+today = datetime.datetime.combine(datetime.date.today(), datetime.time(0, 0))
 
 
-def sendReport(name, url, count):
+def is_article_from_today(article):
+    return article.publish_date == today
+
+
+def get_word_count_in_article(word, article):
+    title_words = re.findall(word, article.title, re.IGNORECASE)
+    text_words = re.findall(word, article.text, re.IGNORECASE)
+
+    return len(title_words) + len(text_words)
+
+
+def send_report(name, url, count):
     report = {"value1": name, "value2": url, "value3": count}
     requests.post("https://maker.ifttt.com/trigger/scraper/with/key/cAb2qiUvSxFnVBTj1dZ7dQ", data=report)
 
 
-for source in sources:
-    name = source["name"]
-    url = source["url"]
-    count = 0
-    news = newspaper.build(url, memoize_articles=False)
+def main():
+    for i in range(len(sources)):
+        name = sources[i]["name"]
+        url = sources[i]["url"]
+        count = 0
+        articles = papers[i].articles
 
-    for article in news.articles[:50]:
-        response = requests.get(article.url)
-        html = response.content
-        soup = BeautifulSoup(html, "lxml")
+        for article in articles:
+            article.parse()
+            if is_article_from_today(article):
+                for code_word in code_words:
+                    count += get_word_count_in_article(code_word, article)
 
-        for codeWord in codeWords:
-            count += len(soup.body.find_all(text=re.compile(codeWord, re.IGNORECASE), recursive=True))
+        send_report(name, url, count)
 
-    sendReport(name, url, count)
+
+main()
